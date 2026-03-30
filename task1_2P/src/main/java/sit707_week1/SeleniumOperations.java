a205c27c332501162251c6b6372b18386bb9ab3b
EvniAkithma <126249687+EvniAkithma@users.noreply.github.com>
2026-03-30 19:46:37 +1100

    Add files via upload

---
+++ task1_2P/src/main/java/sit707_week1/SeleniumOperations.java
@@ -0,0 +1,102 @@
+package sit707_week1;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.Dimension;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+/**
+ * @author Ahsan Habib
+ */
+public class SeleniumOperations {
+    
+    public static void sleep(int sec) {
+        try {
+            Thread.sleep(sec * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void open_chrome_and_close() {
+
+        WebDriverManager.chromedriver().setup();
+
+        System.out.println("Fire up chrome browser.");
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Driver info: " + driver);
+
+        sleep(5);
+
+        driver.quit();
+    }
+
+    public static void open_chrome_maximize_close() {
+
+        WebDriverManager.chromedriver().setup();
+
+        System.out.println("Fire up chrome browser.");
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Driver info: " + driver);
+
+        sleep(5);
+
+        driver.manage().window().maximize();
+
+        sleep(5);
+
+        driver.quit();
+    }
+
+    public static void load_web_page_close() {
+
+        WebDriverManager.chromedriver().setup();
+
+        System.out.println("Fire up chrome browser.");
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Driver info: " + driver);
+
+        sleep(5);
+
+        driver.get("https://selenium.dev");
+
+        sleep(5);
+
+        driver.quit();
+    }
+
+    /*
+     * YOUR ASSIGNMENT FUNCTION
+     */
+    public static void open_chrome_loadpage_resize_close() {
+
+        System.out.println("Hello from 224323742, Evni Akithma Wanniarachchi Kankanamge");
+
+        WebDriverManager.chromedriver().setup();
+
+        System.out.println("Fire up chrome browser.");
+        WebDriver driver = new ChromeDriver();
+
+        System.out.println("Driver info: " + driver);
+
+        sleep(2);
+
+        // Open Google
+        driver.get("https://www.google.com");
+
+        // Resize to 640x480
+        driver.manage().window().setSize(new Dimension(640, 480));
+        sleep(2);
+
+        // Resize to 1280x960
+        driver.manage().window().setSize(new Dimension(1280, 960));
+        sleep(2);
+
+        sleep(2);
+
+        driver.quit();
+    }
+}
